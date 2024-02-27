788be0cd4435d033cba7468581e71542dc194f01
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-02-27 12:51:59 +0200

    Add GpioPin member fucntions

--- src/pin.cpp
+++ src/pin.cpp
@@ -15,3 +15,31 @@ GpioPin::GpioPin(int pinNumber, bool isOutput, bool pullUp) {
         gpio_pull_up(pinNumber);
     }
 }
+
+void GpioPin::setHigh() {
+    gpio_put(this->pinNumber, 1);
+}
+
+void GpioPin::setLow() {
+    gpio_put(this->pinNumber, 0);
+}
+
+void GpioPin::toggle() {
+    if (this->isHigh()) {
+        this->setLow();
+    } else {
+        this->setHigh();
+    }
+}
+
+bool GpioPin::isHigh() {
+    return gpio_get(this->pinNumber);
+}
+
+bool GpioPin::isLow() {
+    return !gpio_get(this->pinNumber);
+}
+
+int GpioPin::getPinNumber() {
+    return this->pinNumber;
+}
