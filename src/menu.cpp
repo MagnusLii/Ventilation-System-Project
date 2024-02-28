49bad3cdf4633101a7a20808f1a7459c8a98cdfc
roni <roni.paloniemi@metropolia.fi>
2024-02-28 16:20:19 +0200

    start of menu

---
+++ src/menu.cpp
@@ -0,0 +1,26 @@
+#include "menu.h"
+
+Menu::Menu(/* args */) {
+
+}
+
+Menu::~Menu() {
+
+}
+
+Menu::update() {
+    
+    switch (this->stage) {
+    case WIFI:
+        // TODO
+        break;
+    case MQTT:
+        // TODO
+        break;
+    case MANUAL:
+        // TODO
+        break;
+    default:
+        break;
+    }
+}
