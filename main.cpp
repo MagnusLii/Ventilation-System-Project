0396f460483b42b5fb86d2007713654d5ad24c4f
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-02-29 13:42:02 +0200

    fix typos

--- main.cpp
+++ main.cpp
@@ -9,9 +9,13 @@
 #define DEBUG_ENABLE
 #include "debugprint.h"
 
+#define EEPROM_BAUD_RATE 1000000
+#define EEPROM_WRITE_CYCLE_MAX_MS 5
 
 int main() {
     stdio_init_all();
+    eeprom_init_i2c(i2c0, EEPROM_BAUD_RATE, EEPROM_WRITE_CYCLE_MAX_MS);
+
 
     LogHandler logHandler(0, 0);
     uint8_t logArray[6] = {1, 3, 3, 7, 0, 0};
@@ -25,11 +29,11 @@ int main() {
     eeprom_read_page(0, logArray2, 8);
 
     for (int i = 0; i < 8; i++) {
-        std::cout << logArray2[i] << " ";
+        std::cout << (int)logArray2[i] << " ";
     }
     std::cout << std::endl;
 
-    int crc = getChecksum(logArray2, 8);
+    int crc = getChecksum(logArray2, 6);
     std::cout << crc << std::endl;
     return 0;
 }
