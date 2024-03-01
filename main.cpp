45ca3ccf68b61712bc82b4e35f7989d3ac642e2c
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-03-01 16:01:36 +0200

    Init eeprom

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 #define EEPROM_WRITE_CYCLE_MAX_MS 5
 
 int main() {
+    stdio_init_all();
+    eeprom_init_i2c(i2c0, EEPROM_BAUD_RATE, EEPROM_WRITE_CYCLE_MAX_MS);
+    
     LogHandler logHandler;
 
     logHandler.zeroAllLogs(LOGTYPE_MSG_LOG);
