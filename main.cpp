a3d89cf195705333f138c479a17fa2da2139ab75
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-03-01 14:39:19 +0200

    emptied main function

--- main.cpp
+++ main.cpp
@@ -13,26 +13,6 @@
 #define EEPROM_WRITE_CYCLE_MAX_MS 5
 
 int main() {
-    stdio_init_all();
-    eeprom_init_i2c(i2c0, EEPROM_BAUD_RATE, EEPROM_WRITE_CYCLE_MAX_MS);
 
-    LogHandler logHandler(0, 0);
-    uint8_t logArray[6] = {1, 3, 3, 7, 0, 0};
-    int loglength = 6;
-    
-    std::cout << getChecksum(logArray, 6) << std::endl;
-
-    enterLogToEeprom(logArray, &loglength, 0);
-
-    uint8_t logArray2[20];
-    eeprom_read_page(0, logArray2, loglength);
-
-    for (int i = 0; i < 8; i++) {
-        std::cout << (int)logArray2[i] << " ";
-    }
-    std::cout << std::endl;
-
-    int crc = getChecksum(logArray2, loglength);
-    std::cout << crc << std::endl;
     return 0;
 }
