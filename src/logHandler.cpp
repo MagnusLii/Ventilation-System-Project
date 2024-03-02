4cc0c34a7c8b875e18eb717db09fa5d91fb53d54
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-03-02 11:07:14 +0200

    test

--- src/logHandler.cpp
+++ src/logHandler.cpp
@@ -137,10 +137,14 @@ void LogHandler::enterLogToEeprom(uint8_t *base8Array, int *arrayLen, const int 
     uint8_t crcAppendedArray[*arrayLen + CRC_LEN];
     memcpy(crcAppendedArray, base8Array, *arrayLen); // copy original array to extended array
     appendCrcToBase8Array(crcAppendedArray, arrayLen);
-    std::cout << "I: " << logAddr/8 << " addr: " << logAddr << " len: " << arrayLen << "Array: ";
+
+    /*
+    std::cout << "I: " << logAddr/8 << " addr: " << logAddr << " len: " << *arrayLen << " Array: ";
     for (int i = 0; i < *arrayLen; i++){
         std::cout << (int)crcAppendedArray[i] << " ";
     }
+    */
+   
     std::cout << std::endl;
     eeprom_write_page(logAddr, crcAppendedArray, *arrayLen);
 }
