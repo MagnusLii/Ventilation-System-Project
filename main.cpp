dd85358917ded6ea4d025cf2d5cc34989f10dded
rsth <riku.helastera@outlook.com>
2024-03-02 19:09:25 +0200

    commhandler continued

--- main.cpp
+++ main.cpp
@@ -16,11 +16,16 @@ int main() {
     stdio_init_all();
 
     DPRINT("Boot");
-    IPStack ipstack("SSID", "PW");
+    const char *ssid = "ATHnet";
+    const char *pw = "Joonas&Rosa";
+    const char *hostname = "192.168.50.153";
+    int port = 1883;
+
+    IPStack ipstack(ssid, pw);
     auto client = MQTT::Client<IPStack, Countdown>(ipstack);
     CommHandler comm_handler(ipstack, client);
-    
-    if (comm_handler.connect_to_server("0.0.0.0", 1883)) {
+
+    if (comm_handler.connect_to_server(hostname, port)) {
         while (true) {
             tight_loop_contents();
         }
@@ -32,17 +37,30 @@ int main() {
         }
     }
 
-    if (comm_handler.subscribe()) {
+    if (comm_handler.subscribe(TopicType::OTHER_TOPIC)) {
         while (true) {
             tight_loop_contents();
         }
     }
 
-    auto mqtt_send = make_timeout_time_ms(5000);
+    if (comm_handler.subscribe(TopicType::CONTROL_TOPIC)) {
+        while (true) {
+            tight_loop_contents();
+        }
+    }
+
+    if (comm_handler.subscribe(TopicType::DATA_TOPIC, "data")) {
+        while (true) {
+            tight_loop_contents();
+        }
+    }
+
+    auto mqtt_send = make_timeout_time_ms(10000);
+    int i = 0;
 
     while (true) {
         if (time_reached(mqtt_send)) {
-            mqtt_send = delayed_by_ms(mqtt_send, 5000);
+            mqtt_send = delayed_by_ms(mqtt_send, 10000);
             if (!comm_handler.verify_connection()) {
                 if (!comm_handler.reconnect()) {
                     while (true) {
@@ -51,15 +69,45 @@ int main() {
                 }
             }
 
-            const char* buf = "Test message";
-            if (comm_handler.publish(buf)) {
-                while (true) {
-                    tight_loop_contents();
+            switch (i) {
+            case 0:
+                if (comm_handler.publish(TopicType::DATA_TOPIC, "Temp 2000 C")) {
+                    while (true) {
+                        tight_loop_contents();
+                    }
                 }
+                i++;
+                break;
+            case 1:
+                if (comm_handler.publish(TopicType::CONTROL_TOPIC, "Fan to 100%")) {
+                    while (true) {
+                        tight_loop_contents();
+                    }
+                }
+                comm_handler.subscribe(TopicType::CONTROL_TOPIC, "control-2");
+                i++;
+                break;
+            case 2:
+                if (comm_handler.publish(TopicType::OTHER_TOPIC, "Never call this number again")) {
+                    while (true) {
+                        tight_loop_contents();
+                    }
+                }
+                comm_handler.unsubscribe(TopicType::OTHER_TOPIC);
+                i = 0;
+                break;
+            default:
+                if (comm_handler.publish(TopicType::OTHER_TOPIC, "Sumting wong")) {
+                    while (true) {
+                        tight_loop_contents();
+                    }
+                }
+                i = 0;
+                break;
             }
         }
 
-        //cyw43_arch_poll();
+        // cyw43_arch_poll();
         client.yield(100);
     }
 
--- src/commhandler.h
+++ src/commhandler.h
@@ -1,19 +1,25 @@
+#pragma once
+
+#include <map>
+
 #include "pico/stdlib.h"
 
 #include "Countdown.h"
 #include "IPStack.h"
 #include "MQTTClient.h"
 
+typedef enum { DATA_TOPIC, CONTROL_TOPIC, OTHER_TOPIC } TopicType;
+
 class CommHandler {
   public:
-    CommHandler(IPStack &ipstack, MQTT::Client<IPStack, Countdown> &client, int MQTTVersion = 4,
-                const char *clientID = "VSP", const char *topic = "vsp-test");
+    CommHandler(IPStack &ipstack, MQTT::Client<IPStack, Countdown> &client, int mqtt_version = 4, const char *client_id = "VSP");
 
     int connect_to_server(const char *hostname, int port);
     int connect_to_broker();
-    int subscribe();
-    int subscribe(const char *topic_name);
-    int publish(const char *payload);
+    int subscribe(TopicType topic_type);
+    int subscribe(TopicType topic_type, const char *topic);
+    int unsubscribe(TopicType topic_type);
+    int publish(TopicType topic_type, const char *payload);
     int verify_connection();
     int reconnect();
 
@@ -21,9 +27,11 @@ class CommHandler {
     IPStack &ipstack;
     MQTT::Client<IPStack, Countdown> &client;
     MQTTPacket_connectData connect_data = MQTTPacket_connectData_initializer;
-    const char *topic;
+    // Something to store topics
+    std::map<TopicType, std::pair<const char *, int>> topics;
 
-    void set_topic(const char *new_topic);
+    void set_topic(TopicType topic_type, const char *new_topic_name);
+    int is_subscribed(TopicType topic_type);
 };
 
 void message_arrived(MQTT::MessageData &data);
