5efb0b97439be7e589c0ba276cabd00813894d37
MagnusLii <111851753+MagnusLii@users.noreply.github.com>
2024-03-04 18:06:40 +0200

    Fixed merge

--- main.cpp
+++ main.cpp
@@ -5,11 +5,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
 #include "hardware/timer.h"
-#include "pico/stdlib.h"
 #include "pico/time.h"
-
 #include "commhandler.h"
 
 #define DEBUG_ENABLE
@@ -17,13 +14,10 @@
 
 #define EEPROM_BAUD_RATE 1000000
 #define EEPROM_WRITE_CYCLE_MAX_MS 5
-
+    
 int main() {
     stdio_init_all();
     eeprom_init_i2c(i2c0, EEPROM_BAUD_RATE, EEPROM_WRITE_CYCLE_MAX_MS);
-int main() {
-    stdio_init_all();
-
     DPRINT("Boot");
     const char *ssid = "ATHnet";
     const char *pw = "Joonas&Rosa";
@@ -119,24 +113,5 @@ int main() {
         // cyw43_arch_poll();
         client.yield(100);
     }
-
-    LogHandler logHandler;
-
-    //logHandler.clearAllLogs(LOGTYPE_MSG_LOG); // clears all logs from addrs 0-2048
-    //logHandler.clearAllLogs(LOGTYPE_REBOOT_STATUS); // clears all logs from addrs 2056-4096
-
-    logHandler.pushLog(TEST); // pushes log to addr 0 (and 4096 ????)
-    logHandler.pushLog(TEST2); // pushes log to addr 8 (and 2056 ????)
-    logHandler.pushLog(TEST2); // pushes log to addr 16 (and 2064 ????)
-    logHandler.pushLog(TEST2); // pushes log to addr 24 (and 2072 ????)
-    logHandler.pushLog(TEST); // pushes log to addr 32 (and 2080 ????)
-
-    logHandler.pushRebootLog(OK); // pushes log to addr 2056 (and 8 ????)
-    logHandler.pushRebootLog(CRASH); // pushes log to addr 2064 (and 16 ????)
-    logHandler.pushRebootLog(FORCED_REBOOT); // pushes log to addr 2072 (and 24 ????)
-    logHandler.pushRebootLog(OK); // pushes log to addr 2080 (and 32 ????)
-
-    printValidLogs(LOGTYPE_MSG_LOG); // prints logs from addrs 0-2048
-    printValidLogs(LOGTYPE_REBOOT_STATUS); // prints logs from addrs 2048-4096
     return 0;
 }
