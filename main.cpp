c5b35f6ff01b73c541abfc4b906d9ea8ae771d1b
roni <roni.paloniemi@metropolia.fi>
2024-03-05 16:17:26 +0200

    pressure workink

--- main.cpp
+++ main.cpp
@@ -16,16 +16,22 @@
 
 #define UART_TX_PIN 4
 #define UART_RX_PIN 5
-#define EEPROM_BAUD_RATE 1000000
-#define EEPROM_WRITE_CYCLE_MAX_MS 5
-
-
 
 
 int main() {
     stdio_init_all();
 
-    eeprom_init_i2c(i2c0, EEPROM_BAUD_RATE, EEPROM_WRITE_CYCLE_MAX_MS);
+    uint sda_pin = 14;
+    uint scl_pin = 15;
+
+    gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+    gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+    gpio_pull_up(sda_pin);
+    gpio_pull_up(scl_pin);
+    gpio_set_dir(sda_pin, GPIO_OUT);
+    gpio_set_dir(scl_pin, GPIO_OUT);
+
+    i2c_init(i2c1, 100000);
 
     shared_uart u{std::make_shared<Uart_instance>(1, 9600, UART_TX_PIN, UART_RX_PIN)};
     shared_modbus mbctrl{std::make_shared<ModbusCtrl>(u)};
@@ -34,22 +40,29 @@ int main() {
     WriteRegister mio(mbctrl, 1, 0, 1);
     ReadRegister dummy(mbctrl, 240, 12345);
 
-    PressureRegister pre(0, 0x81);
+    PressureRegister pre(1, 64);
 
-    int val = pre.get();
+    volatile int val = pre.get();
     DPRINT(val);
 
  
 
-    // uint16_t sped = 1000;
-    // mio.start_transfer(sped);
+    uint16_t sped = 1000;
+    mio.start_transfer(sped);
     //mio2.start_transfer(500);
 
+    while(true) {
+        val = pre.get();
+        DPRINT(val);
+        
+        sleep_ms(1000);
+    }
+
 // while(1) tight_loop_contents();
-    co.start_transfer();
-    while (mbctrl->isbusy()) tight_loop_contents();
-    DPRINT(dummy.is_ok());
-    DPRINT(co.get_float());
+    // co.start_transfer();
+    // while (mbctrl->isbusy()) tight_loop_contents();
+    // DPRINT(dummy.is_ok());
+    // DPRINT(co.get_float());
 
     while(1) tight_loop_contents();
 
--- src/pressure_handler/pressure.cpp
+++ src/pressure_handler/pressure.cpp
@@ -13,13 +13,11 @@ PressureRegister::PressureRegister(uint i2c_index, uint8_t device_address) {
 }
 
 int PressureRegister::get(uint32_t timeout_ms) {
-    i2c_write_blocking(inst, devaddr, &payload, 1, false);
-    uint8_t in[4];
-    i2c_read_blocking_until(inst, devaddr, in, 4, false, delayed_by_ms(get_absolute_time(), timeout_ms));
-    int value = in[0] << 24;
-    value |= in[1] << 16;
-    value |= in[2] << 8;
-    value |= in[3];
+    i2c_write_blocking(i2c1, devaddr, &payload, 1, false);
+    uint8_t in[3];
+    i2c_read_blocking_until(inst, devaddr, in, 3, false, delayed_by_ms(get_absolute_time(), timeout_ms));
+    int value = in[0] << 8;
+    value |= in[1];
+    value = value / 240;
     return value;
-
 }
