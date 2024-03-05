6d82d46fa621b97912116d50706d373b075a9931
roni <roni.paloniemi@metropolia.fi>
2024-03-05 11:11:28 +0200

    pressure

---
+++ src/pressure_handler/pressure.cpp
@@ -0,0 +1,25 @@
+
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "pressure.h"
+
+#define READ_DIFF_CODE 0xF1
+
+
+PressureRegister::PressureRegister(uint i2c_index, uint8_t device_address) {
+    inst = index ? i2c1: i2c0;
+    payload = READ_DIFF_CODE;
+    devaddr = device_address;
+}
+
+PressureRegister::get(uint32_t timeout_ms) {
+    i2c_write_blocking(inst, devaddr, &payload, 1, false);
+    uint8_t in[4];
+    i2c_read_blocking_until(inst, devaddr, in, 4, false, delayed_by_ms(get_absolute_time(), timeout_ms));
+    int value = in[0] << 24;
+    value |= in[1] << 16;
+    value |= in[2] << 8;
+    value |= in[3];
+    return value;
+
+}
---
+++ src/pressure_handler/pressure.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "hardware/i2c.h"
+
+#define PAYLOAD_LEN 3
+
+class PressureRegister {
+    public:
+        PressureRegister(uint i2c_index);
+        int get(uint32_t timeout_ms=500);
+    private:
+        i2c_inst_t *inst;
+        uint8_t payload;
+        uint8_t devaddr;
+};
